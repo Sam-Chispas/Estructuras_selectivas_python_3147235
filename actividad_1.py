c829a1fbebed91176036865e70e0d3b43bf6e519
JuanPabloBeltran <juanpablobeltranrodriguez@gmail.com>
2025-03-19 09:46:24 -0500

    ejercicio entregado

--- actividad_1.py
+++ actividad_1.py
@@ -1,14 +1,17 @@
 '''
 En un sistema de automatizacion industrial, un motor puede estatr encendido o apagado.
-si la temp2ratura supera los 80°, el motor debe apagarse automatizamente. 
-escribir un programa que  controle el estado del motor
+si la temperatura supera los 80°, el motor debe apagarse automaticamente. 
+Escribir un programa que  controle el estado del motor
 y lo apague si la temperatura supera los 80°.
 '''
 # Estado del motor
-Temperatura = 89
+Temperatura =  int (input ("Ingresa la temperatur:"))
 if Temperatura <= 80:
     print("el motor sigue funcionando")
     print("Por lo cual esta encendido")
+    if Temperatura >= 70:
+        print("Advertencia el motor esta llegando a su limite,")
+        print("si llega a 80 se reiniciara el motor para bajar su temperatura")
 else:
     print ("El motor se apago por lo cual dejo de funcioanr")
     print("el reactor se apago")
